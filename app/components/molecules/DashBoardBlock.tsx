9a7347c6a70d639e62bdd1a4ce80c938862ea886
João Marcos de Freitas <joaofreitas.souza@ucsal.edu.br>
2026-09-15 11:05:49 -0300

    Continuação do dashboard, criado moleculas, organismos e template.

--- app/components/molecules/DashBoardBlock.tsx
+++ app/components/molecules/DashBoardBlock.tsx
@@ -8,9 +8,9 @@ interface DashboardBlockProps {
 
 export function DashboardBlock({ value, typeOfData }: DashboardBlockProps) {
     return(
-        <div className="">
-            <Text text=""></Text>
-            <Text text=""></Text>
+        <div className="flex flex-col items-center gap-0.5">
+            <Text text={value.toString()}/>
+            <Text text={typeOfData}/>
         </div>
     );
 }
--- app/components/templates/DashboardTemplate.tsx
+++ app/components/templates/DashboardTemplate.tsx
@@ -1,3 +1,7 @@
+import { DashboardRow } from "../organisms/DashboardRow"
+
 export function DashboardTemplate() {
-    
+    <div>
+        <DashboardRow/>
+    </div>
 }
